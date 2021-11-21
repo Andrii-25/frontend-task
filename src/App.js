58c17bae572ea62ea705f3783626f1bdcf955d80
Andrii-25 <kotsupyr.a@gmail.com>
2021-11-21 18:51:10 +0200

    add table page route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getHeroes } from "./actions/heroes";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TablePage from "./pages/TablePage";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <TablePage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/table" element={<TablePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
