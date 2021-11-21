bdca9681a4880375619392bdfd907eaab159e052
Andrii-25 <kotsupyr.a@gmail.com>
2021-11-21 20:58:53 +0200

    add env.example and update readme

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL || "https://swapi.dev/api/",
   headers: {
     "Content-type": "application/json",
   },
