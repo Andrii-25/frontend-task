896259c57345571ebe13f97d1a87ab42432a21d9
Andrii-25 <kotsupyr.a@gmail.com>
2021-11-23 14:55:39 +0200

    wrap table page into applayout

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,20 +1,11 @@
-import { Layout, message } from "antd";
-import { Content } from "antd/lib/layout/layout";
+import { message } from "antd";
 import { useState, useEffect } from "react";
 import { getHeroes } from "../actions/heroes";
 import { useDispatch, useSelector } from "react-redux";
-import Header from "../components/Header";
-import Sider from "../components/Sider";
+import AppLayout from "../components/AppLayout";
 import Table from "../components/Table";
 
 export default function TablePage() {
-  const contentStyles = {
-    width: "91%",
-    minHeight: "100vh",
-    marginLeft: "9%",
-    padding: "50px",
-  };
-
   const dispatch = useDispatch();
   const heroesData = useSelector((state) => state.heroes);
 
@@ -45,20 +36,19 @@ export default function TablePage() {
   }
 
   return (
-    <Layout>
-      <Header onClick={() => setCollapsed(!collapsed)} />
-      <Layout>
-        <Sider collapsed={collapsed} selectedKeys={["2"]} openKeys={["sub2"]} />
-        <Content className="site-layout-background" style={contentStyles}>
-          <Table
-            data={heroesData}
-            onNext={onNextPage}
-            onPrevious={onPreviousPage}
-            pageNumber={pageNumber}
-            isLoading={isLoading}
-          />
-        </Content>
-      </Layout>
-    </Layout>
+    <AppLayout
+      collapsed={collapsed}
+      onCollapse={() => setCollapsed(!collapsed)}
+      selectedKeys={["2"]}
+      openKeys={["sub2"]}
+    >
+      <Table
+        data={heroesData}
+        onNext={onNextPage}
+        onPrevious={onPreviousPage}
+        pageNumber={pageNumber}
+        isLoading={isLoading}
+      />
+    </AppLayout>
   );
 }
