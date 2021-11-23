5662765f3e6e512de7ef1cc2aa2a620819223463
Andrii-25 <kotsupyr.a@gmail.com>
2021-11-23 14:51:02 +0200

    wrap main page into applayout

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,29 +1,18 @@
-import { Layout } from "antd";
-import { Content } from "antd/lib/layout/layout";
 import { useState } from "react";
-import Header from "../components/Header";
-import Sider from "../components/Sider";
 import Greeting from "../components/Greeting";
+import AppLayout from "../components/AppLayout";
 
 export default function MainPage() {
-  const contentStyles = {
-    width: "91%",
-    minHeight: "100vh",
-    marginLeft: "9%",
-    padding: "50px",
-  };
-
   const [collapsed, setCollapsed] = useState(true);
 
   return (
-    <Layout>
-      <Header onClick={() => setCollapsed(!collapsed)} />
-      <Layout>
-        <Sider collapsed={collapsed} selectedKeys={["1"]} openKeys={["sub1"]} />
-        <Content className="site-layout-background" style={contentStyles}>
-          <Greeting />
-        </Content>
-      </Layout>
-    </Layout>
+    <AppLayout
+      collapsed={collapsed}
+      onCollapse={() => setCollapsed(!collapsed)}
+      selectedKeys={["1"]}
+      openKeys={["sub1"]}
+    >
+      <Greeting />
+    </AppLayout>
   );
 }
